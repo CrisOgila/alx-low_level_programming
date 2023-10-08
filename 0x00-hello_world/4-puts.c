0a0b73c4cf25c18192892b6b2c7b3107162a3159
CrisOgila <ogilacris@gmail.com>
2023-10-08 15:22:30 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+# include <stdio.h>
+/**
+ * main - This is a program with puts function
+ * Return: always 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
