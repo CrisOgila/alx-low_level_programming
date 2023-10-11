17f9fedf1cedaec20a90c540c0806e9176196449
CrisOgila <ogilacris@gmail.com>
2023-10-11 00:01:13 -0700

    software eating

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Return: always 0
+ */
+int main(void)
+{
+	int n, m;
+
+	for (n = 0; n < 100; n++)
+	{
+		for (m = n + 1; m < 100; m++)
+		{
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+
+			if (n != 98 || m != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
