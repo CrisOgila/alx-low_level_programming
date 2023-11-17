619da75a0e82f2a1350372b1b71ef646a0255946
CrisOgila <ogilacris@gmail.com>
2023-11-17 10:23:42 -0800

    fibbonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-unsigned long int fib1 = 1
+unsigned long int fib1 = 1;
 unsigned long int fib2 = 2;
 unsigned long int next;
 int i;
@@ -14,7 +14,7 @@ printf("%lu, ", fib1);
 for (i = 1 ; i < 50 ; i++)
 {
 printf("%lu", fib2);
-next = fib1 + fib2
+next = fib1 + fib2;
 fib1 = fib2;
 fib2 = next;
 if (i != 49)
