1c821f420ecf537f5f49adff7242d72c868c4ce5
CrisOgila <ogilacris@gmail.com>
2023-11-19 22:55:22 -0800

    print diagonal

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,25 +1,25 @@
-#include <"main.h">
+#include "main.h"
 /**
  * print_square - function that prints a square
  * @size: size of square
- * Descrition:use '#' to print square
+ * Descrition: use '#' to print square
  */
-
 void print_square(int size)
 {
-	    int x, y;
-	        y = 0;
-		    if (size < 1)
-			        _putchar('\n');
-		        while (y < size)
-			{
-				    x = 0;
-				        while (x < size)
-						    {
-							            _putchar('#');
-								            x++;
-									            }
-					        _putchar('\n');
-						        y++
-								            }
+	int x, y;
+
+	y = 0;
+	if (size < 1)
+		_putchar('\n');
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('#');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
 }
