398b29bcb2c6a0692ef8e45990b6360cc7b78902
CrisOgila <ogilacris@gmail.com>
2023-11-22 03:14:22 -0800

    comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * main - This is the main function
@@ -5,25 +6,27 @@
  */
 int main(void)
 {
-	int n, m;
+int n, m;
 
-	for (n = 0; n < 100; n++)
-	{
-		for (m = n + 1; m < 100; m++)
-		{
-			putchar((n / 10) + '0');
-			putchar((n % 10) + '0');
-			putchar(' ');
-			putchar((m / 10) + '0');
-			putchar((m % 10) + '0');
-
-			if (n != 98 || m != 99)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar(' ');
-	return (0);
+for (n = 0; n < 100; n++)
+{
+for (m = 0; m < 100; m++)
+{
+if (n < m)
+{
+putchar((n / 10) + 48);
+putchar((n % 10) + 48);
+putchar(' ');
+putchar((m / 10) + 48);
+putchar((m % 10) + 48);
+if (n != 98 || m != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
